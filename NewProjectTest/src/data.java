ddb7a14bc32b2fbb49406cafdb5e082ecf822965
Shilpi Saha <ssaha@kovair.com>
2018-01-12 15:16:00 +0530

    ikkkkk

--- NewProjectTest/src/data.java
+++ NewProjectTest/src/data.java
@@ -2,6 +2,6 @@
 public class data {
 	 public static void main(String args[])
 	  {
-		  System.out.println("Hello world....new..... tesrt..");
+		  System.out.println("Hello world....new..... tesrt..ghiiii");
 	  }
 }
